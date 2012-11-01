fe3fcae26a0b9e3016b69b5c96c7b45f8a16849b
tuxskar <tuxskar@gmail.com>
2012-11-01 15:47:35 +0100

    Added first code folder src

---
+++ src/models/client.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 1, 2012
+
+@author: skar
+'''
+
+class Client(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
