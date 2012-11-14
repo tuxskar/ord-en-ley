3a1cf60c2010f1c8a0473495f749d561a9c08f19
tuxskar <tuxskar@gmail.com>
2012-11-14 13:39:11 +0100

    Added client model class without many to many relations

--- src/models/Models.py
+++ src/models/Models.py
@@ -5,17 +5,32 @@ Created on Nov 14, 2012
 '''
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class Client(Base):
     __tablename__ = 'clients'
     
-    def __init__(self):    
-        pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    surname = Column(String)
+    dni = Column(String, unique=True)
+    email = Column(String)
+    web = Column(String)
+    
+    def __init__(self, name=None, surname=None, 
+                 dni=None, email=None, web=None):
+        self.name = name
+        self.surname = surname
+        self.dni = dni
+        self.email = email
+        self.web = web
+
     
     def __repr__(self):
-        pass
+        return "<Client('%s','%s','%s')>" % \
+                (self.dni, self.name, self.surname)
 
 if __name__ == '__main__':
     pass
