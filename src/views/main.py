6cf27ce1442fc0624734c73bd7968c693c255a1c
tuxskar <tuxskar@gmail.com>
2012-11-23 13:25:02 +0100

    Running first dashboard (main_view) interface

--- src/views/main.py
+++ src/views/main.py
@@ -3,15 +3,48 @@ Created on Nov 17, 2012
 
 @author: skar
 '''
+import sys
+try:  
+    import pygtk  
+    pygtk.require("2.0")  
+except:  
+    pass  
 
-class main(object):
+try:  
+    import gtk  
+    import gtk.glade  
+except:  
+    print("GTK Not Availible")
+    sys.exit(1)
+
+class main_view(object):
     '''
     This is the main view that represent the whole application dashboard
     '''
-
+#    wTree = None
 
     def __init__(self):
         '''
-        TODO
+        Build the main dashboard view using the main_view.glade file 
+        located in interfaces
         '''
-        pass
+        self.filename = "../interfaces/main_view.glade"
+        self.main_window_name = "main_view"
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(self.filename)
+        
+        self.window = self.builder.get_object(self.main_window_name)
+        
+        dic = {
+        "on_main_view_destroy" : self.quit,
+               }
+        self.builder.connect_signals(dic)
+        
+        
+    def quit(self, widget):
+        sys.exit(0)
+
+if __name__ == '__main__':
+    gui = main_view()
+    gui.window.show()
+    gtk.main()
