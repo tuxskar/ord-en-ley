b60c14d4fd86bb52b9b6fd8c195782e16c45cacf
tuxskar <tuxskar@gmail.com>
2012-11-24 10:45:11 +0100

    Added get_all_client feature

--- src/db/db_manager.py
+++ src/db/db_manager.py
@@ -3,15 +3,27 @@ Created on Nov 17, 2012
 
 @author: skar
 '''
+from models import Models
 
 class db_manager(object):
     '''
-    This module is the layer between phisical DB to controller
+    This module is the layer between physical DB to controller
     '''
 
-
-    def __init__(self, session = None):
+    def __init__(self, session = None, user_name = None, user_password=None):
         '''
         This constructor has at least a session object to start petitions agains 
         '''
-        pass
+        if session != None:
+            self.session = session
+        elif user_name != None and user_password != None:
+            self.session = Models.get_session(user_name, user_password)
+        else:
+            self.session = Models.get_session()
+        
+    def get_all_clients(self):
+        return self.session.query(Models.Client).all()
+
+if __name__ == '__main__':
+    dbman = db_manager()
+    print dbman.get_all_clients()
