26137d1ab98f6219035635f40e409805c512d49c
tuxskar <tuxskar@gmail.com>
2012-12-06 12:23:27 +0100

    Added classifiers

--- setup.py
+++ setup.py
@@ -9,22 +9,31 @@ REQUIREMENTS = [
 with open('README') as file:
     long_description = file.read()
 
-setup(name='Ord-en Ley',
+setup(name='ord-en-ley',
       version=version,
-      description='Manager desktop app oriented to lawyers office',
       long_description=long_description,
       author='Oscar Ramirez',
       author_email='tuxskar@gmail.com',
+      description='Manager desktop app oriented to lawyers office',
+      license='GPLv3',
       url='http://github.com/tuxskar/ord-en-ley',
       packages=find_packages(),
       data_files=[('interfaces', [
             'src/interfaces/client_view.glade',
             'src/interfaces/main_view.glade',
             ])],
-      license='GPLv3',
+      install_requires=REQUIREMENTS
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: X11 Applications :: GTK',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python :: 2.7',
+          ]
       entry_points={
           'console_scripts':
           ['ordenley = src.test.run:main']
           },
-      install_requires=REQUIREMENTS
      )
