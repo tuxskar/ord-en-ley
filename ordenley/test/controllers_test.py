59b668163993d58fb8f98d229b09f9d94713a08c
tuxskar <tuxskar@gmail.com>
2012-12-08 13:20:11 +0100

    Added controllers_test file

--- ordenley/test/controllers_test.py
+++ ordenley/test/controllers_test.py
@@ -8,7 +8,17 @@ Created on Dec 8, 2012
 import unittest
 
 class controllers_test(unittest.TestCase):
+    def setUp(self):
+        pass
     pass
+
+    def test_show_client(self):
+        """docstring for test_show_client"""
+        pass
+
+    def test_refresh_client(self):
+        """docstring for test_refresh_client"""
+        pass
 
 if __name__ == '__main__':
     unittest.main()
