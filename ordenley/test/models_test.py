ae5b90e412d3f94488eace9314f7a4451f7c59db
tuxskar <tuxskar@gmail.com>
2012-12-16 21:54:16 +0100

    Some modifications for correct test

--- ordenley/test/models_test.py
+++ ordenley/test/models_test.py
@@ -10,12 +10,13 @@ import unittest
 class models_test(unittest.TestCase):
     def representation_test(self):
         """representation test"""
-        name = "Ana María"
-        surname ="mögg"
+        name = "Ana Mara"
+        surname ="mogg"
         dni = "12341234R"
         email = "anamm@noth.ing"
-        web = "anamögg.com"
+        web = "anamogg.com"
         c = models.Models.Client(name, surname, dni, email, web)
+        print c
         self.assertEqual(str(c), "<Client('%s','%s','%s')>" % (dni, name, surname))
 
 if __name__ == '__main__':
