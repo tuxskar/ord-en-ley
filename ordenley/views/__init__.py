1a7dbaa62dc17e790495100cf70a78c38cd12a2b
tuxskar <tuxskar@gmail.com>
2013-02-08 19:48:21 +0100

    Fixed developer interface path access

--- ordenley/views/__init__.py
+++ ordenley/views/__init__.py
@@ -5,6 +5,6 @@ def get_data_dev(ui_file):
     ROOT = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(ROOT, 'interfaces', ui_file)
 
-def get_data_dev(ui_file):
+def get_data(ui_file):
     dir = os.path.join(sys.prefix,"local/ordenley/interfaces")
     return os.path.join(dir, ui_file)
