f2f58da512b6560834d18450230bc6ff4737f51f
tuxskar <tuxskar@gmail.com>
2013-03-09 13:47:40 +0100

    better indentation for fold, and hide_view implemented

--- ordenley/controllers/controller.py
+++ ordenley/controllers/controller.py
@@ -19,24 +19,24 @@ import gtk
 
 class Controller(object):
     """
-    The controller part of the Model-View-Controller design pattern
+        The controller part of the Model-View-Controller design pattern
     """
-    def __init__(self, user_name=None, user_password=None, ctlr=None):
+    def __init__(self, user_name=None, user_password=None, main_view=None):
         """
         Initialize the controller with db_manager and views needs
         """
         self.db_manager = db.db_manager.db_manager(user_name, user_password)
-        self.main_view = ctlr
+        self.main_view = main_view
         self.__client_views = {}
         self.__new_clients_count = 0
 
     ######### Manage client_view ########
     def show_client(self, kind, client_id=None):
         """
-        Method to show new client_view window
-        kind , data
-        new  , None   : Shows a new client_view window to insert a new client
-        info , client_id : Shows a client_view window with client information
+            Method to show new client_view window
+            kind , data
+            new  , None   : Shows a new client_view window to insert a new client
+            info , client_id : Shows a client_view window with client information
         """
         if kind == "new":
             self.__new_clients_views_count += 1
@@ -52,37 +52,40 @@ class Controller(object):
     def client_returned_values(self, modification=None, mod_obj=None, 
                                 new_data=None, old_id=None):
         """
-        Method to get the returned values from client_view
-        This method is called when you finish modify a client in client_view and 
-        you want to return all the modifications
-        Args   : modification , mod_obj  , new_data , old_id
-        values : None         , None     , None     , None       __ No modification at all
-               : client       , delete   , client   , client.id
-               :              , modified ,          ,            __ modify or delete client with old id old_id
-               : address      , delete   , address  , address.id
-               :              , modified ,          ,            __ modify or delete address with old id old_id
+            Method to get the returned values from client_view
+            This method is called when you finish modify a client in client_view and 
+            you want to return all the modifications
+            Args   : modification , mod_obj  , new_data , old_id
+            values : None         , None     , None     , None       __ No modification at all
+                   : client       , delete   , client   , client.id
+                   :              , modified ,          ,            __ modify or delete client with old id old_id
+                   : address      , delete   , address  , address.id
+                   :              , modified ,          ,            __ modify or delete address with old id old_id
         """
-        pass
+        # TODO: write code ...
 
-    def hide_client_view(self, view):
+    def hide_view(self, view):
         """
-        Method to hide the view with id equal to client_id
-        arg: view
-        values: "main"    -- hide the main_view
-                client_id -- hide the client_view with id=client_id
+            Method to hide the view with id view=client_id or main_view
+            arg: view
+            values: "main"    -- hide the main_view
+                    client_id -- hide the client_view with id=client_id
         """
-        pass
+        if view=="main":
+            self.main_view.hide()
+        else:
+            self.__client_views.get(view).hide()
 
     ######## Manage main_view ########
     def connect_main_view(self, view):
         """
-        Method to connect controller with unique main_view
+            Method to connect controller with unique main_view
         """
         self.main_view = view
 
     def fill_up_main_view(self):
         """
-        Method to fill up main_view client_tree
+            Method to fill up main_view client_tree
         """
         for client in self.db_manager.cm.get_client_columns():
             row_client = [client[0], client[1], client[2]]
@@ -91,9 +94,9 @@ class Controller(object):
         
     def add_client_row(self, client):
         """
-        Method to add the client to the main_view client_tree
+            Method to add the client to the main_view client_tree
         """
-        pass
+        # TODO: write code ...
 
 
 
